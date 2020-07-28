dd74d5b09187fe89315d6b123fd51f8acdd2dd73
Angel Reyes <angelmrg2000@hotmail.com>
2020-07-28 11:53:59 -0600

    initial commit

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export class UserModel {
+    constructor(
+        public name:string,
+        public username: string,
+        public email: string,
+        public password: string,
+        public image: string
+    ){}   
+}
